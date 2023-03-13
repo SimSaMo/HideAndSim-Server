67be2d760f17fa399f00d261de2139ed765ceabb
kikuke <kkkjs0814@naver.com>
2023-03-13 16:22:28 +0900

    [Add] gitignore folder

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kikuke/DV/EpollBase_CppGameServer
-# Build directory: /home/kikuke/DV/EpollBase_CppGameServer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("KikukeLib")
-subdirs("Server")
-subdirs("ClientTester")
--- build/Server/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kikuke/DV/EpollBase_CppGameServer/Server
-# Build directory: /home/kikuke/DV/EpollBase_CppGameServer/build/Server
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
